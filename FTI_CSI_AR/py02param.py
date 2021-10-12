5977ee9332e78dd18f3440b987673bce15ce419e
yoshisatoh <73818039+yoshisatoh@users.noreply.github.com>
2021-10-12 19:42:54 +0900

    Delete FTI_CSI_AR directory

--- FTI_CSI_AR/py02param.py
+++
@@ -1,8 +0,0 @@
-# moving average days for asset mean returns (mu), standard deviations (sigma), and correlations (rho)
-dma = (250 * 5)
-
-# percentile rank: moving average lookback days for FTI
-FTI_pct_rank_dma = 30
-
-# percentile rank: time period of percentile rank calculation
-FTI_pct_rank_dtp = (250 * 5)
